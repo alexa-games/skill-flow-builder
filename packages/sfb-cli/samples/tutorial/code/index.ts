93a1b6b6aea1966c27a73e6c916d22f21db8f595
toorooji <toorooji@amazon.com>
2021-04-13 10:22:35 -0700

    Add a debug flag for logs, scrub logs for sensitive data

--- packages/sfb-cli/samples/tutorial/code/index.ts
+++ packages/sfb-cli/samples/tutorial/code/index.ts
@@ -28,7 +28,11 @@ const configAccessor = new ConfigAccessor(require(path.resolve("abcConfig", "abc
  * Skill handler (Request Entry Point)
  */
 export async function handler(event: any, context: any, callback: any) {
-	console.log(`[INFO] Request Received: ${JSON.stringify(event, null, 4)}`);
+    const debug = configAccessor.isDebug();
+
+    if(debug)
+	  console.log(`[INFO] Request Received: ${JSON.stringify(event, ((key, value) =>
+	    key === 'apiAccessToken' ? null : value), 4)}`);
 
 	const customExtensionLoader = new ExtensionLoader({
 		locale: event.request.locale,
@@ -57,6 +61,7 @@ export async function handler(event: any, context: any, callback: any) {
 	
 	const response = await skill.invoke(event, context);
 
-	console.log(`[INFO] Outgoing Response: ${JSON.stringify(response, null, 4)}`);
+    if(debug)
+    	console.log(`[INFO] Outgoing Response: ${JSON.stringify(response, null, 4)}`);
 	return response;
 }
